9712d2151c6a290515a77a7912ab09a454b40d26
Josh Kleymeyer <josh.kleymeyer@gmail.com>
2019-11-01 10:45:52 -0400

    11/1 push

---
+++ GradeConverter/GradeConverter/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GradeConverter {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Welcome to the Grade Converter");
+
+            String choice = "y";
+            while (choice.Equals("y")) {
+                Console.WriteLine("Enter Numerical Grade: ");
+                String input = Console.ReadLine();
+                int NumGrade = int.Parse(input);
+                String LetterGrade = " ";
+
+                if(NumGrade >= 88) {
+                    LetterGrade = "A";
+                } else if (NumGrade >= 80) {
+                    LetterGrade = "B";
+                } else if (NumGrade >= 67) {
+                    LetterGrade = "C";
+                } else if (NumGrade >= 60) {
+                    LetterGrade = "D";
+                } else {
+                    LetterGrade = "F";
+                }
+
+                Console.WriteLine("Letter Grade:" + LetterGrade);
+                Console.WriteLine("Continue? (y/n)");
+                choice = Console.ReadLine();
+            }
+            Console.WriteLine("Goodbye");
+        }
+    }
+}
