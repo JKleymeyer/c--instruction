0950024ac9dc18068f7f75fa803135519820aa40
Josh Kleymeyer <josh.kleymeyer@gmail.com>
2019-11-01 10:40:10 -0400

    11/1 push

---
+++ StudentRegistration/StudentRegistration/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace StudentRegistration {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("student Registration Form");
+            Console.WriteLine();
+
+            Console.WriteLine("Enter first name: ");
+            String FName = Console.ReadLine();
+            Console.WriteLine("Enter last name: ");
+            String LName = Console.ReadLine();
+            Console.WriteLine("Enter year of birth: ");
+            String Birth = Console.ReadLine();
+
+
+            Console.WriteLine();
+            Console.WriteLine("Welcome " + FName + " " + LName);
+            Console.WriteLine("Your registration is complete");
+            Console.WriteLine("Your temporary password is: " + FName + "*" + Birth);
+        } 
+    }
+}
---
+++ TableOfPowers/TableOfPowers/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace TableOfPowers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Squares and Cubes table");
+
+
+            String choice = "y";
+            while (choice.Equals("y"))
+            {
+                Console.WriteLine("Enter an integer");
+                int integer = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Number\t" + "Square\t" + "Cube");
+                Console.WriteLine("======\t" + "======\t" + "====");
+
+                for (int i = 1; i <= integer; i++)
+                {
+                    int square = i + i;
+                    int cube = i + i + i;
+
+                    Console.WriteLine(+i+ "\t" + square+ "\t" + cube);
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Continue? (y/n)");
+                choice = Console.ReadLine();
+            }
+
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
